f8a1f7f759510e8f8ad44490127c748cfbd9ef3d
ysyesilyurt <yesilyurt.selim@metu.edu.tr>
2019-09-05 13:44:03 +0300

    triplets

--- hackerrank/IPKit/dicts-hashmaps/countTriplets.py
+++ hackerrank/IPKit/dicts-hashmaps/countTriplets.py
@@ -4,45 +4,45 @@ import math
 
 """
 Sample Input
-6 3
-1 3 9 9 27 81
+5 2
+1 2 1 2 4
 Sample Output
-6
+3
 """
 
 def countTriplets(arr, r):
-    arr = sorted(arr)
-    dct = {}
-    count = 0
-    for item in arr:
-        if item == 1 or item % r == 0:
+    numDct = {}
+    res = 0
+    if r == 1:
+        for i in arr:
             try:
-                dct[item] += 1
+                numDct[i] += 1
             except KeyError:
-                dct[item] = 1
-    i = arr[0]
-    if arr[0] == arr[-1]:
-        count = int(math.factorial(dct[i]) / (math.factorial(dct[i] - 3) * 6))
-        return count
-    elif r == 1:
-        for num in dct:
-            if dct[num] > 2:
-                count += int(math.factorial(dct[num]) / (math.factorial(dct[num] - 3) * 6))
-        return count
-    while i < arr[-1]:
-        try:
-            count += dct[i] * dct[i * r] * dct[i * r * r]
-            i *= r
-        except KeyError:
-            i *= r
-    return count
+                numDct[i] = 1
+        for i in numDct:
+            res += int((numDct[i] * (numDct[i]-1) * (numDct[i]-2)) / 6)
+    else:
+        duoDct = {}
+        for i in arr:
+            if i/r in numDct:
+                try:
+                    duoDct[(i / r, i)] += numDct[i/r]
+                except KeyError:
+                    duoDct[(i / r, i)] = numDct[i/r]
+            if (i / r / r, i / r) in duoDct:
+                res += duoDct[(i / r / r, i / r)]
+            try:
+                numDct[i] += 1
+            except KeyError:
+                numDct[i] = 1
+    return res
 
 
 if __name__ == '__main__':
-    #res = countTriplets([1,3,9,9,27,81], 3)
-    #print(res)
-    with open("inp.txt", "r") as inp:
-        line = list(map(int, inp.readline().split(" ")))
-        res = countTriplets(line, 3)
-        print(res)
+    res = countTriplets([1,2,1,2,4], 2)
+    print(res)
+    # with open("inp.txt", "r") as inp:
+    #     line = list(map(int, inp.readline().split(" ")))
+    #     res = countTriplets(line, 3)
+    #     print(res)
 
